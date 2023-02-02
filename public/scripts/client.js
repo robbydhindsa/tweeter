3eb90311f190f954ea204a13ac2c51db6c21efda
Robby Dhindsa <dhindsa.1996@gmail.com>
2023-02-01 17:57:11 -0800

    create dynamic tweet rendering for tweet container

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,81 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function () {
+
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
+  const renderTweets = function(tweets) {
+    tweets.forEach(tweet => {
+      $('.tweet-container').append(createTweetElement(tweet));
+    });
+  }
+  // render function using map
+  // const renderTweets = function(tweets) {
+  //   const result = tweets.map(tweet => {
+  //     $('.tweet-container').append(createTweetElement(tweet));
+  //   });
+  // }
+
+  const createTweetElement = function(tweet) {
+    let $tweet = $(`
+      <article>
+      <div class="tweeter-user">
+        <div>
+          <span>${tweet.user.avatars}</span>
+          <span>${tweet.user.name}</span>
+        </div>
+        <div>
+          <span>${tweet.user.handle}</span>
+        </div>
+      </div>
+      
+      <div class="actual-tweet">
+        <p>${tweet.content.text}</p>
+      </div>
+      
+      <div class="timestamp-engagement">
+        <div>
+          <span>${tweet.created_at}</span>
+        </div>
+        <div>
+          <span>
+            <i class="fa-solid fa-flag"></i>
+            <i class="fa-solid fa-retweet"></i>
+            <i class="fa-solid fa-heart"></i>
+          </span>
+        </div>
+      </div>
+      </article>
+    `)
+    return $tweet;
+  };
+
+
+  renderTweets(data);
+
+});
+
